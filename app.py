5f5ab3d9805df4298ddea4e2cfe875e9676c4ee2
olgapf02 <olgapf02@gmail.com>
2023-05-11 12:49:00 +0200

    graficas enseñadas

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ Created on Mon Feb 13 16:09:40 2023
 @author: Olga
 """
 # importaciones
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
-import requests
 import matplotlib.pyplot as plt
 import pandas as pd
 import json
+import calendar
+import requests
 # ###################################################################
 # https://api.nasa.gov/planetary/apod?api_key=dD3rq2KBZ3IonoPglO50EyY5R1ielcmBT1gbO1Jv   
 # aqui estaremos haciendo una consulta a la api de la nasa con nuestra apikey que nos saldra en formato json
@@ -134,37 +135,54 @@ def get_img_ano():
     # aqui creamos un dataframe a partir de las columnas que queremos y con los datos recogidos en la api
     df = pd.DataFrame(lista_imagenes, columns=["title", "date", "explanation", "url", "tipo"])
     # print(df)
-
     # escriviremos un archivo csv para tener el df mas manejable.
     # df.to_csv('imagenes-año.csv', index=False)
-
     # por ultimo en este render daremos una variable para que printe nuestro df en el html
     return render_template('fotos_ano.html',lista =df.to_html())
+
 ###################################################################################################
-# enseñar la grafica con un app.route
-@app.route('/grafica')
+# en esta funcion crearemos las diferentes grficas que necesitamos 
 def grafica():
+    # abriremos el csv para poder extraer los datos para la grafica
     df = pd.read_csv('imagenes-año.csv')
+    # este codigo servira para hacer una grafica de barras de que tipo de imagen hay mas en un año
     # Calcular el recuento de cada tipo de imagen
-    conteo_tipos = df["tipo"].value_counts()
+    # conteo_tipos = df["tipo"].value_counts()
+    # # Creacion de la  gráfica de barras
+    # fig, ax = plt.subplots()
+    # ax.bar(conteo_tipos.index, conteo_tipos.values)
+    # # Personalizacion de la gráfica
+    # ax.set_title("Tipos de imágenes en el año")
+    # ax.set_xlabel("Tipo de imagen")
+    # ax.set_ylabel("Cantidad")
+    # # Guardar la gráfica como archivo PNG
+    # # plt.savefig('grafica.png')
+    # # Cerrar la figura
+    # plt.close()  
+#############################
+# a lo que este pedazo de codigo nos servira para enseñar una grafica de quesitos de que tipo de foto hay mas en cada mes de un año
+    df['date'] = pd.to_datetime(df['date'])# convierte la columna 'date' en formato de fecha utilizando la función 'pd.to_datetime' 
+    df['mes'] = df['date'].dt.month    # utiliza la propiedad 'dt.month' de la columna 'date' para extraer el mes correspondiente a cada fecha y lo asigna a la nueva columna 'mes'
 
-    # Creacion de la  gráfica de barras
-    fig, ax = plt.subplots()
-    ax.bar(conteo_tipos.index, conteo_tipos.values)
-
-    # Personalizacion de la gráfica
-    ax.set_title("Tipos de imágenes en el año")
-    ax.set_xlabel("Tipo de imagen")
-    ax.set_ylabel("Cantidad")
-
-    # Guardar la gráfica como archivo PNG
-    plt.savefig('grafica.png')
-
-    # Cerrar la figura
-    plt.close()
-    # En este ejemplo, la línea plt.savefig('grafica.png') guarda la gráfica en un archivo llamado "grafica.png" en el directorio actual.
-
-    return render_template('grafica.html')
+    for mes in range(1, 13):# rango de los 12 meses del año
+        nombre_mes = calendar.month_name[mes]
+        datos_mes = df[df['mes'] == mes]
+        tipo_mas_frecuente = datos_mes['tipo'].mode()[0]
+        datos_tipo = datos_mes['tipo'].value_counts()
+        plt.pie(datos_tipo.values, labels=datos_tipo.index, autopct='%1.1f%%')
+        plt.title(f'Tipo de imagen más frecuente en el mes de {nombre_mes}')
+        # plt.savefig(f'grafica_mes_{mes}.png')
+        plt.close()
+        
+###################################################################################################
+# # enseñar las graficas a traves de un form con un app.route
+@app.route('/formulario',methods=['GET','POST'])
+def seleccionar_imagen():
+    if request.method == 'POST':
+        opcion = request.form['opciones']
+        return render_template('formulario.html', imagen=opcion)
+    else :
+        return render_template('formulario.html')
 
 
 ###################################################################
@@ -172,5 +190,4 @@ app.run(host='Localhost', port=5000, debug=False)
 
 ###################################################################################################
 # mejoras
-# crear una grafica de cada mes
-#cojer cada mes y enseñar su grafica  con un formulario
+# cambiar el menu con js
